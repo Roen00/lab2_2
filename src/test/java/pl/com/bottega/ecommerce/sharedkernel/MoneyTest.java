536f6a92d1303ac6d2af4635961e0aa18153194e
Rafal Zelek <r.zelek@oberthur.com>
2017-01-14 15:26:40 +0100

    added money test which checks happy path of sum of two moneys

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -1,7 +1,24 @@
 package pl.com.bottega.ecommerce.sharedkernel;
 
+import org.junit.Test;
+
+import java.util.Currency;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class MoneyTest {
+
+    @Test
+    public void sumOf5MoneyAnd3MoneyOfTheSameCurrencyShouldGive8MoneyWithTheSameCurrency(){
+        //Given
+        final Currency currency = Currency.getInstance("USD");
+        final Money money5 = new Money(5, currency);
+        final Money money3 = new Money(3, currency);
+        //When
+        final Money result = money5.add(money3);
+        //Then
+        assertThat(result, is(new Money(8, currency)));
+    }
 
 }
