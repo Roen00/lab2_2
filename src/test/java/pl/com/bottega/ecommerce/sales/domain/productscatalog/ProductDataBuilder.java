73c1f88bb9c13cb75414b8026076993e74c091a6
Rafal Zelek <r.zelek@oberthur.com>
2017-01-21 15:45:07 +0100

    added product data builder in test package

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/ProductDataBuilder.java
@@ -0,0 +1,43 @@
+package pl.com.bottega.ecommerce.sales.domain.productscatalog;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
+
+public class ProductDataBuilder {
+    private Id productId;
+    private Money price;
+    private String name;
+    private ProductType type;
+    private Date snapshotDate;
+
+    public ProductDataBuilder setProductId(Id productId) {
+        this.productId = productId;
+        return this;
+    }
+
+    public ProductDataBuilder setPrice(Money price) {
+        this.price = price;
+        return this;
+    }
+
+    public ProductDataBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProductDataBuilder setType(ProductType type) {
+        this.type = type;
+        return this;
+    }
+
+    public ProductDataBuilder setSnapshotDate(Date snapshotDate) {
+        this.snapshotDate = snapshotDate;
+        return this;
+    }
+
+    public ProductData createProductData() {
+        return new ProductData(productId, price, name, type, snapshotDate);
+    }
+}
