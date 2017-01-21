7cfa37c42feafa13fe2617d6f77cbe0b72b828be
Rafal Zelek <r.zelek@oberthur.com>
2017-01-21 16:48:46 +0100

    added taxAmount to tax policy stub constructor

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxPolicyStub.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxPolicyStub.java
@@ -4,8 +4,14 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class TaxPolicyStub implements TaxPolicy{
+    private final Money taxAmount;
+
+    public TaxPolicyStub(Money taxAmount) {
+        this.taxAmount = taxAmount;
+    }
+
     @Override
     public Tax calculateTax(ProductType productType, Money net) {
-        return new Tax(net, "tax_description");
+        return new Tax(taxAmount, "tax_description");
     }
 }
